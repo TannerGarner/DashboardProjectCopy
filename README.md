e98cd516e7ab956351bede215942c9cd63581e81
tannergarner <garnert.w.14@gmail.com>
2024-09-14 21:21:49 -0600

    Update from other repository

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DashboardProject
+In this project we had to recreate a dashboard.
+
+# Breakpoints
+The major breakpoints for this website are 0px to 780px, 781px to 1040px, and 1041px and up.
---
+++ script.js
@@ -0,0 +1,4 @@
+const sideNav = document.getElementsByClassName("sideNav")[0];
+
+function showSideNav() {sideNav.style.display = "flex";}
+function closeSideNav() {sideNav.style.display = "none";}
